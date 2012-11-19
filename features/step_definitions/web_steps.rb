8205f964912cb4c835c3222545e06da88225cc98
Micah Geisel <micah@botandrose.com>
2012-11-19 00:39:24 -0800

    simplify cucumber web steps some more.

--- features/step_definitions/web_steps.rb
+++ features/step_definitions/web_steps.rb
@@ -41,27 +41,27 @@ When /^(.*) within (.*[^:]):$/ do |other_step, parent, table_or_string|
   with_scope(parent) { step "#{other_step}:", table_or_string }
 end
 
-Given /^(?:|I )am on (.+)$/ do |page_name|
+Given /^I am on (.+)$/ do |page_name|
   visit path_to(page_name)
 end
 
-When /^(?:|I )go to (.+)$/ do |page_name|
+When /^I go to (.+)$/ do |page_name|
   visit path_to(page_name)
 end
 
-When /^(?:|I )press "([^"]*)"$/ do |button|
+When /^I press "(.*?)"$/ do |button|
   click_button(button)
 end
 
-When /^(?:|I )follow "([^"]*)"$/ do |link|
+When /^I follow "(.*?)"$/ do |link|
   click_link(link)
 end
 
-When /^(?:|I )fill in "([^"]*)" with "([^"]*)"$/ do |field, value|
+When /^I fill in "(.*?)" with "(.*?)"$/ do |field, value|
   fill_in(field, :with => value)
 end
 
-When /^(?:|I )fill in "([^"]*)" for "([^"]*)"$/ do |value, field|
+When /^I fill in "(.*?)" for "(.*?)"$/ do |value, field|
   fill_in(field, :with => value)
 end
 
@@ -76,29 +76,29 @@ end
 # TODO: Add support for checkbox, select og option
 # based on naming conventions.
 #
-When /^(?:|I )fill in the following:$/ do |fields|
+When /^I fill in the following:$/ do |fields|
   fields.rows_hash.each do |name, value|
     step %{I fill in "#{name}" with "#{value}"}
   end
 end
 
-When /^(?:|I )select "([^"]*)" from "([^"]*)"$/ do |value, field|
+When /^I select "(.*?)" from "(.*?)"$/ do |value, field|
   select(value, :from => field)
 end
 
-When /^(?:|I )check "([^"]*)"$/ do |field|
+When /^I check "(.*?)"$/ do |field|
   check(field)
 end
 
-When /^(?:|I )uncheck "([^"]*)"$/ do |field|
+When /^I uncheck "(.*?)"$/ do |field|
   uncheck(field)
 end
 
-When /^(?:|I )choose "([^"]*)"$/ do |field|
+When /^I choose "(.*?)"$/ do |field|
   choose(field)
 end
 
-When /^(?:|I )attach the file "([^"]*)" to "([^"]*)"$/ do |path, field|
+When /^I attach the file "(.*?)" to "(.*?)"$/ do |path, field|
   attach_file(field, File.expand_path(path))
 end
 
@@ -120,50 +120,7 @@ Then /^I should not see \/(.*?)\/$/ do |regexp|
   page.should have_no_xpath('//*', :text => regexp)
 end
 
-Then /^the "([^"]*)" field(?: within (.*))? should contain "([^"]*)"$/ do |field, parent, value|
-  with_scope(parent) do
-    field = find_field(field)
-    field_value = (field.tag_name == 'textarea') ? field.text : field.value
-    field_value.should =~ /#{value}/
-  end
-end
-
-Then /^the "([^"]*)" field(?: within (.*))? should not contain "([^"]*)"$/ do |field, parent, value|
-  with_scope(parent) do
-    field = find_field(field)
-    field_value = (field.tag_name == 'textarea') ? field.text : field.value
-    field_value.should_not =~ /#{value}/
-  end
-end
-
-Then /^the "([^"]*)" checkbox(?: within (.*))? should be checked$/ do |label, parent|
-  with_scope(parent) do
-    field_checked = find_field(label)['checked']
-    field_checked.should be_true
-  end
-end
-
-Then /^the "([^"]*)" checkbox(?: within (.*))? should not be checked$/ do |label, parent|
-  with_scope(parent) do
-    field_checked = find_field(label)['checked']
-    field_checked.should be_false
-  end
-end
- 
-Then /^(?:|I )should be on (.+)$/ do |page_name|
+Then /^I should be on (.+)$/ do |page_name|
   current_path = URI.parse(current_url).path
   current_path.should == path_to(page_name)
-end
-
-Then /^(?:|I )should have the following query string:$/ do |expected_pairs|
-  query = URI.parse(current_url).query
-  actual_params = query ? CGI.parse(query) : {}
-  expected_params = {}
-  expected_pairs.rows_hash.each_pair{|k,v| expected_params[k] = v.split(',')} 
-  
-  actual_params.should == expected_params
-end
-
-Then /^show me the page$/ do
-  save_and_open_page
 end
