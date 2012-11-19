5f30ffdb5b41c96c9119578a8cb0663ccdf1a187
Micah Geisel <micah@botandrose.com>
2012-11-19 00:54:17 -0800

    break apart calendar helper.

--- app/helpers/calendar_helper.rb
+++ app/helpers/calendar_helper.rb
@@ -1,7 +1,6 @@
 require 'date'
 
 module CalendarHelper
-
   def calendar(options = {}, &block)
     block ||= Proc.new {|d| nil}
 
@@ -16,54 +15,50 @@ module CalendarHelper
     first = Date.civil(options[:year], options[:month], 1)
     last = first.end_of_month
 
-    first_weekday = 0
-    last_weekday = 6
-
-    haml_tag :tbody do
-      haml_tag :tr do
-        # start out the week with last month's days
-        first.beginning_of_week(:sunday).upto(first - 1.day) do |d|
-          haml_tag :td, :class => "otherMonth" do
-            haml_concat d.day
-          end
-        end
-
-        # this months days
-        first.upto(last) do |d|
-          classes = []
-          classes << "today" if d.today?
-          haml_tag :td, :id => "day_#{d.day}", :class => classes.compact.join(' ') do
-            haml_concat d.mday
-            haml_concat capture_haml(d, &block)
-          end
-          haml_concat "</tr><tr>" if d.saturday? unless d == last
-        end
-
-        # finish out the week with next month's days
-        unless last.saturday?
-          (last + 1).upto(last.end_of_week(:sunday)) do |d|
-            haml_tag :td, :class => "otherMonth" do
-              haml_concat d.day
-            end
-          end
-        end
-      end
-    end
+    build first, last, options, &block
   end
 
   private
 
-    def days_between(first, second)
-      if first > second
-        second + (7 - first)
-      else
-        second - first
+  def build first, last, options, &block
+    haml_tag :tbody do
+      haml_tag :tr do
+        last_month_days first # start out the week with last month's days
+        this_month_days first, last, &block
+        next_month_days last # finish out the week with next month's days
       end
     end
+  end
 
-    def saturday?(date)
-      date.wday == 6
+  def last_month_days first
+    first.beginning_of_week(:sunday).upto(first - 1.day) do |d|
+      haml_tag :td, :class => "otherMonth" do
+        haml_concat d.day
+      end
     end
+  end
+
+  def this_month_days first, last, &block
+    first.upto(last) do |d|
+      classes = []
+      classes << "today" if d.today?
+      haml_tag :td, :id => "day_#{d.day}", :class => classes.compact.join(' ') do
+        haml_concat d.mday
+        haml_concat capture_haml(d, &block)
+      end
+      haml_concat "</tr><tr>" if d.saturday? unless d == last
+    end
+  end
+
+  def next_month_days last
+    unless last.saturday?
+      (last + 1).upto(last.end_of_week(:sunday)) do |d|
+        haml_tag :td, :class => "otherMonth" do
+          haml_concat d.day
+        end
+      end
+    end
+  end
 end
 
 module CalendarHelper
@@ -77,9 +72,8 @@ module CalendarHelper
 
   private
 
-    def link_to_month(direction, date, options = {})
-      date += (direction == :next ? 1 : -1).month
-      link_to direction, calendar_path(:year => date.year, :month => date.month), options
-    end
+  def link_to_month(direction, date, options = {})
+    date += (direction == :next ? 1 : -1).month
+    link_to direction, calendar_path(:year => date.year, :month => date.month), options
+  end
 end
-
