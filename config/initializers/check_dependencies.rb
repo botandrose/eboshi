9fc1909a47eeb53fbaabd4daaf37a1435d8dce7d
Micah Geisel <micah@botandrose.com>
2012-12-01 11:49:14 -0800

    ensure imagemagick is installed.

--- config/initializers/check_dependencies.rb
+++ config/initializers/check_dependencies.rb
@@ -1,3 +1,4 @@
 if RUBY_PLATFORM !~ /darwin/i
   raise "External dependency *wkhtmltopdf* not found! Please install." unless `which wkhtmltopdf` and $?.success?
+  raise "External dependency *imagemagick* not found! Please install." unless `which identify` and $?.success?
 end
