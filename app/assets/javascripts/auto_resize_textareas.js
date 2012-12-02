b38579ad694f727ca608a0510a36201324757014
Micah Geisel <micah@botandrose.com>
2012-12-01 18:13:13 -0800

    fix auto resize textarea.

--- app/assets/javascripts/auto_resize_textareas.js
+++ app/assets/javascripts/auto_resize_textareas.js
@@ -1,6 +1,6 @@
 //= require jquery.livequery
 
-$(function() {
+$(window).load(function () {
   $("td.notes textarea").livequery(function() {
     new AutoResizeTextArea.init(this)
   })
@@ -17,59 +17,34 @@ var AutoResizeTextArea = {
     $(el).css("overflowX", "auto")
       
     // create a new element that will be used to track the dimensions  
-    var dummy = document.createElement('div');  
+    var dummy = document.createElement('pre');  
     el.parentNode.appendChild(dummy);
   
     // hide the created div away  
-    $(dummy).css({ position: 'absolute', top: '0px', left: '-99999px' })
-//    $(dummy).css("position", 'absolute')
-//    $(dummy).css("margin-left", '6px')
-//    $(dummy).css("top", "282px")
+    $(dummy).css({ position: 'absolute', left: "-99999px", "white-space": "pre-wrap" })
     $(dummy).addClass("dummy")
+    var width = $(el).innerWidth()
+    $(dummy).css('width', width)
 
     dummy.innerHTML = 'a';
     var lineHeight = dummy.offsetHeight;  
 
-    dummy.innerHTML = el.value.replace(/\n/g, '<br>');  
-      
     var checkExpandContract = function() {
-      // hack width to fix, 3px too big for some god damned reason
-      var width = $(el).css('width')
-      if(width.charAt(width.length-1) == "x") {  
-        width = parseInt(width.substring(0,width.length-2))   
-      }
-      $(dummy).css('width', width-3+"px")
-
       // place text inside the element in a new var called html  
       // transfer content to dummy      
-      var html = el.value.replace(/\n/g, '<br>');  
-      if(dummy.innerHTML != html) {  
-        dummy.innerHTML = html;
+      dummy.innerHTML = el.value;
           
-        // skip if the height from our element matches the dummy height
-        if (el.offsetHeight != dummy.offsetHeight + lineHeight) {
-          // ensure minimum height
-          if (dummy.offsetHeight < originalHeight) {
-            el.style.height = originalHeight+'px';
-          } else {
-            el.style.height = (dummy.offsetHeight + lineHeight) + 'px';
-          }
-        }
+      // ensure minimum height
+      if (dummy.offsetHeight < originalHeight) {
+        height = originalHeight;
+      } else {
+        height = dummy.offsetHeight + lineHeight;
       }
-    }
-    
-    // Startup
-    var expandElement = function()  {
-        interval = window.setInterval(function() {checkExpandContract()}, 250);
-    }
-    
-    var contractElement = function() {
-        clearInterval(interval);
+      $(el).css({ height: height });
     }
     
     // Put eventListeners to our elements
-    $(el).focus(expandElement);
-    $(el).blur(contractElement);
-    $(window).load(checkExpandContract)
+    $(el).keyup(checkExpandContract);
+    checkExpandContract();
   }
 }
