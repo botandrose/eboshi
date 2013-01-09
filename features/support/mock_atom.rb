708aaa55a7e3320cd6f94fd71e0c8d93afe9c73c
Micah Geisel <micah@botandrose.com>
2013-01-09 13:30:43 -0800

    rm cruft.

--- features/support/mock_atom.rb
+++
@@ -1,2 +0,0 @@
-require File.expand_path(File.dirname(__FILE__) + '/../../spec/support/mock_atom')
-mock_atom
--- spec/support/mock_atom.rb
+++
@@ -1,3 +0,0 @@
-def mock_atom
-  FileUtils.cp "#{Rails.root}/db/blog_feed.atom.fixture", "#{Rails.root}/db/blog_feed.atom"
-end
