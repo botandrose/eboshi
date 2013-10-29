29767e5a263eabedab810ebb0d0b069ec329dcad
Micah Geisel <micah@botandrose.com>
2013-10-29 13:56:26 -0700

    use Y-m-d format for dates.

--- spec/models/line_item_spec.rb
+++ spec/models/line_item_spec.rb
@@ -11,38 +11,38 @@ describe LineItem do
 
   describe ".on_date" do
     it "filters out line items not on supplied date" do
-      yesterday = Work.make :start => Time.zone.parse("6/30/2012 23:59:59")
-      today = Work.make :start => Time.zone.parse("7/1/2012 00:00:00")
-      tomorrow = Work.make :start => Time.zone.parse("7/2/2012 00:00:00")
-      LineItem.on_date(Time.zone.parse("7/1/2012 23:59:59")).should == [today]
+      yesterday = Work.make :start => Time.zone.parse("2012-6-30 23:59:59")
+      today = Work.make :start => Time.zone.parse("2012-7-1 00:00:00")
+      tomorrow = Work.make :start => Time.zone.parse("2012-7-2 00:00:00")
+      LineItem.on_date(Time.zone.parse("2012-7-1 23:59:59")).should == [today]
     end
   end
 
   describe ".on_week" do
     it "filters out line items not on supplied date's week" do
-      # 7/1/2012 is a sunday
-      last_week = Work.make :start => Time.zone.parse("6/30/2012 23:59:59")
-      this_week = Work.make :start => Time.zone.parse("7/1/2012 00:00:00")
-      next_week = Work.make :start => Time.zone.parse("7/8/2012 00:00:00")
-      LineItem.on_week(Time.zone.parse("7/7/2012 23:59:59")).should == [this_week]
+      # 2012-7-1 is a sunday
+      last_week = Work.make :start => Time.zone.parse("2012-6-30 23:59:59")
+      this_week = Work.make :start => Time.zone.parse("2012-7-1 00:00:00")
+      next_week = Work.make :start => Time.zone.parse("2012-7-8 00:00:00")
+      LineItem.on_week(Time.zone.parse("2012-7-7 23:59:59")).should == [this_week]
     end
   end
 
   describe ".on_month" do
     it "filters out line items not on supplied date's month" do
-      last_month = Work.make :start => Time.zone.parse("6/30/2012 23:59:59")
-      this_month = Work.make :start => Time.zone.parse("7/1/2012 00:00:00")
-      next_month = Work.make :start => Time.zone.parse("8/1/2012 00:00:00")
-      LineItem.on_month(Time.zone.parse("7/31/2012 23:59:59")).should == [this_month]
+      last_month = Work.make :start => Time.zone.parse("2012-6-30 23:59:59")
+      this_month = Work.make :start => Time.zone.parse("2012-7-1 00:00:00")
+      next_month = Work.make :start => Time.zone.parse("2012-8-1 00:00:00")
+      LineItem.on_month(Time.zone.parse("2012-7-31 23:59:59")).should == [this_month]
     end
   end
 
   describe ".on_year" do
     it "filters out line items not on supplied date's year" do
-      last_year = Work.make :start => Time.zone.parse("12/31/2011 23:59:59")
-      this_year = Work.make :start => Time.zone.parse("6/1/2012 00:00:00")
-      next_year = Work.make :start => Time.zone.parse("1/1/2013 00:00:00")
-      LineItem.on_year(Time.zone.parse("12/31/2012 23:59:59")).should == [this_year]
+      last_year = Work.make :start => Time.zone.parse("2011-12-31 23:59:59")
+      this_year = Work.make :start => Time.zone.parse("2012-6-1 00:00:00")
+      next_year = Work.make :start => Time.zone.parse("2013-1-1 00:00:00")
+      LineItem.on_year(Time.zone.parse("2012-12-31 23:59:59")).should == [this_year]
     end
   end
 
