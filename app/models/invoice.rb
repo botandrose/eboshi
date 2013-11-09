3c5a916077cbe18720d0ff3f8648d1fefdf917f5
Micah Geisel <micah@botandrose.com>
2013-11-08 16:26:34 -0800

    deleting an invoice puts its lineitems back in the queue.

--- app/models/invoice.rb
+++ app/models/invoice.rb
@@ -11,7 +11,7 @@
 
 class Invoice < ActiveRecord::Base
   belongs_to :client
-  has_many :line_items
+  has_many :line_items, :dependent => :nullify
   has_many :payments, :dependent => :destroy, :order => 'created_at DESC'
   
   validates_presence_of :client, :date
