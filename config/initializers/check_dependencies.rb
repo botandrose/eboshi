f2c3724c99c2cbea7a7d65bd5165db7557e9b9eb
Micah Geisel <originofstorms@gmail.com>
2013-11-09 20:56:09 -0800

    Only warn on dependency check failure so we can run on travis.

--- config/initializers/check_dependencies.rb
+++ config/initializers/check_dependencies.rb
@@ -1,4 +1,2 @@
-if RUBY_PLATFORM !~ /darwin/i
-  raise "External dependency *wkhtmltopdf* not found! Please install." unless `which wkhtmltopdf` and $?.success?
-  raise "External dependency *imagemagick* not found! Please install." unless `which identify` and $?.success?
-end
+puts "External dependency *wkhtmltopdf* not found! Please install." unless `which wkhtmltopdf` and $?.success?
+puts "External dependency *imagemagick* not found! Please install." unless `which identify` and $?.success?
