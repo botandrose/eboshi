b3e61946c4972fc74e7f4feeb091f1782077af87
Micah Geisel <micah@botandrose.com>
2013-11-13 10:17:59 -0800

    schema.rb.

--- db/schema.rb
+++ db/schema.rb
@@ -16,8 +16,8 @@ ActiveRecord::Schema.define(:version => 20131113174931) do
   create_table "assignments", :force => true do |t|
     t.integer  "user_id"
     t.integer  "client_id"
-    t.datetime "created_at", :null => false
-    t.datetime "updated_at", :null => false
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
   add_index "assignments", ["client_id"], :name => "index_assignments_on_client_id"
