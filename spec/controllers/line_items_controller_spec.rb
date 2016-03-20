f15f814f5a361d7399a7f55e4d516e2d56ea9808
Micah Geisel <micah@botandrose.com>
2016-03-19 17:41:30 -0700

    cover LineItemsController.

---
+++ spec/controllers/line_items_controller_spec.rb
@@ -0,0 +1,22 @@
+require "spec_helper"
+
+describe LineItemsController do
+  include ControllerSpecHelpers
+
+  let(:client) { FactoryGirl.create(:client) }
+  let(:work) { FactoryGirl.create(:work, client: client, timestamp: 2, notes: "test") }
+
+  describe "on update" do
+    it "updates notes if note is newer than existing" do
+      put :update, client_id: client.id, id: work.id, line_item: { timestamp: 3, notes: "testing" }
+      response.should be_success
+      work.reload.notes.should == "testing"
+    end
+
+    it "rejects notes update if note is older than existing" do
+      put :update, client_id: client.id, id: work.id, line_item: { timestamp: 1, notes: "testing" }
+      response.should be_success
+      work.reload.notes.should == "test"
+    end
+  end
+end
